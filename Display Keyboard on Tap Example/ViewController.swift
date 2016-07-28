38b329b6299dbc9803192f2848406a981f52a1ac
Stephen Groom <stephen.groom@me.com>
2016-07-28 20:16:19 +0100

    Initial Commit

---
+++ Display Keyboard on Tap Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Display Keyboard on Tap Example
+//
+//  Created by Stephen Groom on 28/07/2016.
+//  Copyright © 2016 Stephen Groom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
